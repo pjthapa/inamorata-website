3a8d497a402fb31529c1c3f50dc5e9322d08d51b
Prasanna T <thapapj19@gmail.com>
2022-01-07 21:02:56 -0500

    added css to page three, images are responive

--- src/components/cocktailCard.js
+++ src/components/cocktailCard.js
@@ -6,9 +6,6 @@ function CreateCard(props){
     return(
         <div className="card">
             <img src={props.image} alt={`${props.image}`} className="img"/>
-            <div className="container">
-                <h4>{props.description}</h4>
-            </div>
         </div>
     )
 }
