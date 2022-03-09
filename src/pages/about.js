61fa411dc97f4ca86317e9adfe6c52a4cb9d9fa8
Prasanna T <thapapj19@gmail.com>
2022-03-09 11:02:26 -0500

    added logo

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,6 +1,7 @@
 import React from "react";
 import {Link} from "react-router-dom";
 import cassie_making_cocktails from '../images/cassie_making_cocktails.png'
+import Logo from "../components/logo";
 
 function AboutPage() {
     return(
@@ -8,8 +9,9 @@ function AboutPage() {
             <div className="navbar container">
                 <Link to="/" className="nav home">HOME</Link>
                 <Link to="/Event" className="nav event">EVENTS</Link>
-                <Link to="/About" className="nav about">ABOUT</Link> 
+                <Link to="/About" className="nav about"style={{background :"#f3feff", paddingLeft:"2rem", paddingRight:"2rem"}}>ABOUT</Link> 
             </div>
+            <Logo /> 
             <div className="bodyContent">
                 <img src={cassie_making_cocktails}  alt="Cassie Making a Cocktail"/>
             </div>
